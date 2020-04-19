9d26cc29b7f9b73903078f8a8c6a8d8bb1c915a1
Rokibul Islam <rrcucse.11@gmail.com>
2020-04-19 06:02:02 +0600

    start

---
+++ database/migrations/2020_04_18_231201_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('code');
+            $table->string('product_image');
+            $table->string('buy_date');
+            $table->string('expire_date');
+            $table->string('buying_price');
+            $table->string('selling_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
